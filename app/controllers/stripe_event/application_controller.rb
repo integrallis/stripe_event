f395db579b22297dc565a21c4ffced8a64162616
Christopher Baran <cb@chrisbaran.net>
2012-08-13 17:14:53 -0400

    Fixed nil error for @event instance variable.  Added .idea to .gitignore to ignore files made by rubymine IDE.

--- app/controllers/stripe_event/application_controller.rb
+++ app/controllers/stripe_event/application_controller.rb
@@ -1,14 +1,5 @@
 module StripeEvent
   class ApplicationController < ActionController::Base
-    
-    # Authentication
-    before_filter do
-      begin
-        @event = Stripe::Event.retrieve(params[:id])
-      rescue Stripe::StripeError
-        head :unauthorized
-      end
-    end
-    
+
   end
 end
--- app/controllers/stripe_event/webhook_controller.rb
+++ app/controllers/stripe_event/webhook_controller.rb
@@ -1,8 +1,25 @@
 module StripeEvent
   class WebhookController < ApplicationController
+    skip_before_filter :verify_authenticity_token, :if => :xml_request?
+
+    # Authentication
+    before_filter do
+      begin
+        @event = Stripe::Event.retrieve(params[:id])
+      rescue Stripe::StripeError
+        head :unauthorized
+      end
+    end
+
     def event
       StripeEvent.publish(@event)
       head :ok
+    end
+
+    protected
+
+    def xml_request?
+      request.format.xml?
     end
   end
 end
