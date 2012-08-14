c104f9233357925fa0ded2900af462944e0bf706
Robert Bousquet <robert@robertbousquet.com>
2012-08-14 16:53:52 -0700

    Remove @event instance variable and update specs

--- app/controllers/stripe_event/webhook_controller.rb
+++ app/controllers/stripe_event/webhook_controller.rb
@@ -1,8 +1,8 @@
 module StripeEvent
   class WebhookController < ActionController::Base
     def event
-      @event = Stripe::Event.retrieve(params[:id])
-      StripeEvent.publish(@event)
+      event = Stripe::Event.retrieve(params[:id])
+      StripeEvent.publish(event)
       head :ok
     rescue Stripe::StripeError
       head :unauthorized
