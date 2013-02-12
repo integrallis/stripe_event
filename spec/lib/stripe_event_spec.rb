9bffed11cd9b7ed806043cee0b8a1f5e8d08b850
Daniel Whalen <dwhalen@integrallis.com>
2013-02-11 20:59:30 -0700

    Reference the described class within specs

--- spec/lib/stripe_event_spec.rb
+++ spec/lib/stripe_event_spec.rb
@@ -4,17 +4,17 @@ describe StripeEvent do
   let(:event_type) { StripeEvent::TYPE_LIST.sample }
 
   it "backend defaults to AS::Notifications" do
-    expect(StripeEvent.backend).to eq ActiveSupport::Notifications
+    expect(described_class.backend).to eq ActiveSupport::Notifications
   end
 
   it "registers a subscriber" do
-    subscriber = StripeEvent.subscribe(event_type) { |e| }
+    subscriber = described_class.subscribe(event_type) { |e| }
     subscribers = subscribers_for_type(event_type)
     expect(subscribers).to eq [subscriber]
   end
 
   it "registers subscribers within a parent block" do
-    StripeEvent.setup do
+    described_class.setup do
       subscribe('invoice.payment_succeeded') { |e| }
     end
     subscribers = subscribers_for_type('invoice.payment_succeeded')
@@ -25,21 +25,21 @@ describe StripeEvent do
     event = { :type => event_type }
 
     expect { |block|
-      StripeEvent.subscribe(event_type, &block)
-      StripeEvent.publish(event)
+      described_class.subscribe(event_type, &block)
+      described_class.publish(event)
     }.to yield_with_args(event)
   end
 
   it "uses Stripe::Event as the default event retriever" do
     Stripe::Event.should_receive(:retrieve).with('1')
-    StripeEvent.event_retriever.call(:id => '1')
+    described_class.event_retriever.call(:id => '1')
   end
 
   it "allows setting an event_retriever" do
     params = { :id => '1' }
 
-    StripeEvent.event_retriever = Proc.new { |arg| arg }
-    event = StripeEvent.event_retriever.call(params)
+    described_class.event_retriever = Proc.new { |arg| arg }
+    event = described_class.event_retriever.call(params)
     expect(event).to eq params
   end
 end
