f6e6246c86b42585cb9e206615d53a609baff397
Daniel Whalen <dwhalen@integrallis.com>
2013-03-19 16:17:14 -0700

    Cleanup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,14 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'webmock/rspec'
 
-ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
+support_dir = File.join(File.dirname(__FILE__), '../spec/support/**/*.rb')
+Dir[support_dir].each { |f| require f }
 
 RSpec.configure do |config|
-  config.include(FixtureHelper)
-  config.include(ActiveSupportHelper)
+  config.include FixtureHelper
+  config.include ActiveSupportHelper
 
   config.order = 'random'
 
@@ -22,13 +17,13 @@ RSpec.configure do |config|
   end
 
   config.before do
-    @_event_retriever = StripeEvent.event_retriever
-    @_notifier = StripeEvent.backend.notifier
-    StripeEvent.backend.notifier = @_notifier.class.new
+    @event_retriever = StripeEvent.event_retriever
+    @notifier = StripeEvent.backend.notifier
+    StripeEvent.backend.notifier = @notifier.class.new
   end
 
   config.after do
-    StripeEvent.event_retriever = @_event_retriever
-    StripeEvent.backend.notifier = @_notifier
+    StripeEvent.event_retriever = @event_retriever
+    StripeEvent.backend.notifier = @notifier
   end
 end
