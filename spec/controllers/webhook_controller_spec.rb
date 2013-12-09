0a09d1fa52ee71f0b1ff1b6ab195ed273ed72411
Danny Whalen <daniel.r.whalen@gmail.com>
2013-12-09 10:11:44 -0800

    Cleanup controller specs

--- spec/controllers/webhook_controller_spec.rb
+++ spec/controllers/webhook_controller_spec.rb
@@ -1,37 +1,28 @@
 require 'spec_helper'
 
 describe StripeEvent::WebhookController do
-  def event_post(params)
+  def webhook(params)
     post :event, params.merge(:use_route => :stripe_event)
   end
 
   it "succeeds with valid event data" do
     stub_event('evt_charge_succeeded')
 
-    event_post :id => 'evt_charge_succeeded'
-    expect(response).to be_success
+    webhook :id => 'evt_charge_succeeded'
+    expect(response.code).to eq '200'
   end
 
   it "denies access with invalid event data" do
     stub_event('evt_invalid_id', 404)
 
-    event_post :id => 'evt_invalid_id'
+    webhook :id => 'evt_invalid_id'
     expect(response.code).to eq '401'
   end
 
   it "ensures user-generated Stripe exceptions pass through" do
-    StripeEvent.setup do
-      subscribe('charge.succeeded') { |e| raise Stripe::StripeError }
-    end
+    StripeEvent.subscribe('charge.succeeded') { |e| raise Stripe::StripeError }
     stub_event('evt_charge_succeeded')
 
-    expect {event_post :id => 'evt_charge_succeeded'}.to raise_error(Stripe::StripeError)
-  end
-
-  it "succeeds with a custom event retriever" do
-    StripeEvent.event_retriever = Proc.new { |params| params }
-
-    event_post :id => '1'
-    expect(response).to be_success
+    expect { webhook :id => 'evt_charge_succeeded' }.to raise_error(Stripe::StripeError)
   end
 end
