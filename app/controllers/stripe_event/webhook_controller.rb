50e4ba07f2965e7cad80627e321428f376da5784
Brent Royal-Gordon <brent@architechies.com>
2015-02-22 14:22:56 -0800

    Actually test authentication secret

--- app/controllers/stripe_event/webhook_controller.rb
+++ app/controllers/stripe_event/webhook_controller.rb
@@ -1,5 +1,12 @@
 module StripeEvent
   class WebhookController < ActionController::Base
+    before_action do
+      StripeEvent.authentication_secret.nil? or \
+        authenticate_or_request_with_http_basic do |username, password|
+          password == StripeEvent.authentication_secret
+        end
+    end
+    
     def event
       StripeEvent.instrument(params)
       head :ok
