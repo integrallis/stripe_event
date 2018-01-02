65c432714aaab932256bc18c1d7f64e695b5d558
Chubchenko <artem.chubchenko@gmail.com>
2018-01-03 00:55:21 +0200

    Update version file

--- lib/stripe_event.rb
+++ lib/stripe_event.rb
@@ -1,40 +1,37 @@
-require "active_support/notifications"
-require "stripe"
-require "stripe_event/engine" if defined?(Rails)
-require "stripe_event/configuration"
-require "stripe_event/exceptions"
-require "stripe_event/namespace"
-require "stripe_event/notification_adapter"
-require "stripe_event/notification_adapter"
-require "stripe_event/version"
+require 'active_support/notifications'
+require 'stripe'
+require 'stripe_event/engine' if defined?(Rails)
+require 'stripe_event/configuration'
+require 'stripe_event/exceptions'
+require 'stripe_event/namespace'
+require 'stripe_event/notification_adapter'
+require 'stripe_event/notification_adapter'
+require 'stripe_event/version'
 
 module StripeEvent
   class << self
     def configure(&block)
       Configuration.instance.configure(&block)
     end
-    alias :setup :configure
+    alias setup configure
 
     def instrument(event)
       event = event_filter.call(event)
-      backend.instrument namespace.call(event.type), event if event
+      return unless event
+      backend.instrument(namespace.call(event.type), event)
     end
 
     def subscribe(name, callable = Proc.new)
-      backend.subscribe namespace.to_regexp(name), adapter.call(callable)
+      backend.subscribe(namespace.to_regexp(name), adapter.call(callable))
     end
 
     def all(callable = Proc.new)
-      subscribe nil, callable
+      subscribe(nil, callable)
     end
 
     def listening?(name)
       namespaced_name = namespace.call(name)
       backend.notifier.listening?(namespaced_name)
-    end
-
-    def configuration
-      Configuration.instance
     end
 
     def method_missing(method_name, *arguments, &block)
@@ -45,8 +42,14 @@ module StripeEvent
       end
     end
 
-    def respond_to?(method_name, include_private = false)
+    def respond_to_missing?(method_name, include_private = false)
       configuration.respond_to?(method_name.to_sym) || super
+    end
+
+    private
+
+    def configuration
+      Configuration.instance
     end
   end
 end
--- lib/stripe_event/version.rb
+++ lib/stripe_event/version.rb
@@ -1,3 +1,49 @@
 module StripeEvent
-  VERSION = "2.0.0"
+  module Version
+    module_function
+
+    KEYS = %i[major minor patch].freeze
+
+    # Major version.
+    #
+    # @return [Integer]
+    def major
+      2
+    end
+
+    # Minor version.
+    #
+    # @return [Integer]
+    def minor
+      0
+    end
+
+    # Patch version.
+    #
+    # @return [Integer]
+    def patch
+      0
+    end
+
+    # Returns a hash representation of version.
+    #
+    # @return [Hash]
+    def to_h
+      Hash[KEYS.zip(to_a)]
+    end
+
+    # Returns a string representation of version.
+    #
+    # @return [Array]
+    def to_a
+      [major, minor, patch]
+    end
+
+    # Returns an array representation of version.
+    #
+    # @return [String]
+    def to_s
+      to_a.join('.')
+    end
+  end
 end
